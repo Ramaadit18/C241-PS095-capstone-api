f62635d3f64f597884099f3cdfeb06b3bc0f91e9
Ramaadit18 <ramaawp10@gmail.com>
2024-06-17 18:41:22 +0700

    Update route.js

--- src/userService/route.js
+++ src/userService/route.js
@@ -2,21 +2,19 @@ const express = require('express');
 const verifyToken = require('../middleware/authMiddleware');
 const {
   createUser,
-  getUsers,
   getUserById,
   updateUsername,
   updateUserEmail,
   updateUserPassword,
-  deleteUser
+  deleteUserById
 } = require('./handler');
 const router = express.Router();
 
 router.post('/', createUser);
-router.get('/', getUsers);
 router.get('/:id', verifyToken, getUserById);
 router.patch('/username/:id', verifyToken, updateUsername);
 router.patch('/email/:id', verifyToken, updateUserEmail);
 router.patch('/password/:id', verifyToken, updateUserPassword);
-router.delete('/:id', verifyToken, deleteUser);
+router.delete('/:id', verifyToken, deleteUserById);
 
 module.exports = router;

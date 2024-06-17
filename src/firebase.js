a76007abee466a79b22a473479f6a30128b392dd
Ramaadit18 <ramaawp10@gmail.com>
2024-06-17 18:40:50 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-//const { getAuth } = require('firebase-admin/auth');
 
 const serviceAccount = require('../serviceAccountKey.json');
 
@@ -9,6 +8,5 @@ initializeApp({
 });
 
 const firestore = getFirestore();
-//const auth = getAuth();
 
 module.exports = { firestore };
